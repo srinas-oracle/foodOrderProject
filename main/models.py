e97509bcdf3ec9166935d4733ba34d39661950fd
srinas-oracle <sri.nandita.s@oracle.com>
2022-02-10 19:59:41 +0530

    First commit

---
+++ main/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from main import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), default='default.jpg', nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    orders = db.relationship('Order', backref='author', lazy=True)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurantname = db.Column(db.String(20), nullable=False)
+    active = db.Column(db.String(5), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String(30), nullable=False, default='default.jpg')
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    location = db.Column(db.String(20), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(15), nullable=False)
+    image = db.Column(db.String(30), nullable=False, default='default.jpg')
+    foods = db.relationship('Food', backref='provider', lazy=True)
---
+++ main/routes.py
@@ -0,0 +1,167 @@
+from main.models import User, Restaurant, Food, Order
+from flask import render_template, url_for, flash, redirect, request
+from main.forms import RegistrationForm, UpdateAccountForm, LoginForm
+from main import app, db, bcrypt
+from datetime import datetime
+from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+import json
+import time
+
+toms = [
+        {
+            'restaurantname': 'Burger King',
+            'price': 570,
+            'image': 'burgerking.png',
+            'date_created': datetime.utcnow().strftime('%m %d %y - %H:%M:%S'),
+            'selecteditems': [
+                {
+                    'name': 'Margarita Pizza',
+                    'price': 250,
+                    'image': 'food1.jpeg',
+                    'quantity': 1
+                },
+                {
+                    'name': 'Italian Farm Pizza',
+                    'price': 320,
+                    'image': 'food1.jpeg',
+                    'quantity': 1
+                }
+
+            ]
+        },
+        {
+            'restaurantname': 'Cheesecake Factory',
+            'price': 250,
+            'image': 'cheesecakefactory.png',
+            'date_created': datetime.utcnow().strftime('%m %d %y - %H:%M:%S'),
+            'selecteditems': [
+                {
+                    'name': 'Margarita Pizza',
+                    'price': 250,
+                    'quantity': 1
+                 }
+            ]
+        }
+]
+foods = {
+    'restaurantname': 'Burger King',
+    'selecteditems': [
+        {
+            'name': 'Margarita Pizza',
+            'price': 250,
+            'image': 'food1.jpeg',
+            'quantity': 1
+        },
+        {
+            'name': 'Italian Farm Pizza',
+            'price': 320,
+            'image': 'food1.jpeg',
+            'quantity': 1
+        }
+    ]
+}
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/restaurants")
+@login_required
+def restaurants():
+    restaurants_list = Restaurant.query.all()
+    return render_template('list_restaurants.html', title='Restaurants', restaurants=restaurants_list)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(url_for(next_page[1:])) if next_page else redirect(url_for('home'))
+        else:
+            flash('Please check email and password', 'danger')
+    return render_template('login.html', form=form, title='Login')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/order/<int:restaurantid>")
+def order(restaurantid):
+    restaurant = Restaurant.query.get(int(restaurantid))
+    restaurants_list = Restaurant.query.all()
+    must = {
+        'name' : restaurant.name
+    }
+    return render_template('order.html', title=restaurant.name, restaurants=restaurants_list, name=restaurant.name, foods=restaurant.foods, must=must)
+
+@app.route("/hello", methods=['GET','POST'])
+def hello():
+    if request.method == 'POST':
+        global response
+        response = request.get_json()
+        return 'OK', 200
+
+@app.route("/cart", methods=['GET', 'POST'])
+@login_required
+def cart():
+    total = 0
+    return render_template('cart.html', title='Cart', foods=foods, total=total)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_fn = save_picture(form.picture.data)
+            current_user.image_file = picture_fn
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile/'+current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/orders")
+def orders():
+    return render_template('orders.html', title='Orders', tom=toms)
+
+@app.route("/category/<string:categoryname>")
+def category(categoryname):
+    lists = Restaurant.query.filter_by(type=categoryname).all()
+    return render_template('category.html', title=categoryname, restaurants=lists)
